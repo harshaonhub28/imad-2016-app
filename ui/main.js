f01864aeaf25c9f54629c4f30513ce04b5db902a
harshaonhub28 <hvk.2803@gmail.com>
2016-09-30 21:53:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,9 +9,9 @@ button.onclick = function(){
         if(request.readystate === XMLHttpRequest.DONE){
             
             if (request.status ===200)  {
-            var counter = request.responseText;
-            var span = document.getElementById("count");
-            span.innerHTML = counter.toString();
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
             }
         }
     }
