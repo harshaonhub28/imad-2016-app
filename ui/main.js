661af315b7f68e60cf1a23bfd45e1d1864b31fd0
harshaonhub28 <hvk.2803@gmail.com>
2016-10-04 13:51:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,19 @@ request.open('GET','http://harshaonhub28.imad.hasura-app.io/counter',true);
 request.send(null);  
 };
 
+//capture the name
+var nameInput = document.getElementById('name');
+var sname = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //send name to server and get results back
+    
+    //capture the names and display them
+    var names = ['eric','stan','kyle','kenny'];
+    var list='';
+    for(var i=0; i<names.length;i++){
+        list += '<li>'+name[i]+'</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+}
