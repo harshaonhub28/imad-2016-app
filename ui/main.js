64a099f4e2f94c52a3c2f4c749234df412206183
harshaonhub28 <hvk.2803@gmail.com>
2016-10-04 14:18:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,14 +20,28 @@ request.send(null);
 };
 
 //capture the name
-var nameInput = document.getElementById('name');
-var sname = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
+    
+    var nameInput = document.getElementById('name');
+    var sname = nameInput.value;
+
     //send name to server and get results back
+     var request =new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if (request.status ===200)  {
+                var names = request.responseText;
+                names = JSON.parse(names);
+            }
+        }
+    }
+  
+request.open('GET','http://harshaonhub28.imad.hasura-app.io/submit-name/:'+sname,true);
+request.send(null);  
     
     //capture the names and display them
-    var names = ['eric','stan','kyle','kenny'];
     var list='';
     for(var i=0; i<names.length;i++){
         list += '<li>'+names[i]+'</li>';
