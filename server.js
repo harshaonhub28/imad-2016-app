c14e8aa942ca608923a1ec82401257d1cf924450
harshaonhub28 <hvk.2803@gmail.com>
2016-10-25 12:49:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/submit-name/:sname',function(req,res){
      res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
     var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
