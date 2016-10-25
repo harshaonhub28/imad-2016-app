f338634e031066825cd3edc1111e5f32da0e9af7
harshaonhub28 <hvk.2803@gmail.com>
2016-10-25 12:56:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,8 +73,8 @@ app.get('/counter',function(req,res){
 });
 
 var names=[];
-app.get('/submit-name/:sname',function(req,res){
-     var name = req.params.sname;
+app.get('/submit-name/:what',function(req,res){
+     var name = req.params.what;
      names.push(name);
      res.send(JSON.stringify(names));
 });
