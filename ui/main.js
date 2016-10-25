1786e7f928700eb24889b715da905744db4d2c6b
harshaonhub28 <hvk.2803@gmail.com>
2016-10-25 16:39:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,6 +46,7 @@ request.send(null);
     var list='';
     for(var i=0; i<names.length;i++){
         list += '<li>'+names[i]+'</li>';
+        console.log(names[i]);
     }
     console.log(list);
     var ul = document.getElementById('namelist');
